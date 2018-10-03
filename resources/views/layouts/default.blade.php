ee9e385812d18c85b2d03d1ba51efa25e9f5a68d
umar farooque khan <uffarooque73@gmail.com>
2018-10-03 19:40:13 +0530

    first

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 5.5 CRUD example</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+ 
+<div class="container">
+    @yield('content')
+</div>
+ 
+</body>
+</html>
